7b568403507d9d1541a748811d408cb2d557fddf
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-12 16:44:48 +0100

    Crear mètodes validateHealthCard() i validateSurname(), per validar les dades escrites en els camps Health Card, First Surname i Second Surname.

--- src/main/java/com/example/projectetqs/controller/MainController.java
+++ src/main/java/com/example/projectetqs/controller/MainController.java
@@ -1,4 +1,5 @@
 package com.example.projectetqs.controller;
+import com.example.projectetqs.AlertDialog;
 import com.example.projectetqs.model.Visit;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -100,33 +101,34 @@ public class MainController implements Initializable {
 
   @FXML
   public void addVisit() {
-    String dateTime=(getDateVisit()+" "+getHour()+":"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-    if((getHour()>=0 && getHour()<=9) && (getMinutes()>=0 && getMinutes()<=9))
-      dateTime = (getDateVisit()+" "+"0"+getHour()+":"+"0"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-    else{
-      if(getHour()>=0 && getHour()<=9)
-        dateTime = (getDateVisit()+" "+"0"+getHour()+":"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    if (validationHealthCard() && validateSurname()){
+      String dateTime=(getDateVisit()+" "+getHour()+":"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+      if((getHour()>=0 && getHour()<=9) && (getMinutes()>=0 && getMinutes()<=9))
+        dateTime = (getDateVisit()+" "+"0"+getHour()+":"+"0"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+      else{
+        if(getHour()>=0 && getHour()<=9)
+          dateTime = (getDateVisit()+" "+"0"+getHour()+":"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 
-      if(getMinutes()>=0 && getMinutes()<=9)
-        dateTime = (getDateVisit()+" "+getHour()+":"+"0"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        if(getMinutes()>=0 && getMinutes()<=9)
+          dateTime = (getDateVisit()+" "+getHour()+":"+"0"+getMinutes()).formatted(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+      }
+      visit = new Visit(getHealthCard(), getName(), getFirstSurname(), getSecondSurname(), getGender(),
+          getDateBirth().atStartOfDay(),
+          LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+      visit.saveVisitToJSON();
+
+      textHealthCard.clear();
+      textName.clear();
+      textFirstSurname.clear();
+      textSecondSurname.clear();
+      maleButton.isSelected();
+      femaleButton.isSelected();
+      textDateBirth.getEditor().clear();
+      textDateVisit.getEditor().clear();
+      valueFactorySpinner();
+      setCellTable();
+      loadData(); //Llegir dades de JSON, i afegir-les a la taulas
     }
-
-    visit = new Visit(getHealthCard(), getName(), getFirstSurname(), getSecondSurname(), getGender(),
-        getDateBirth().atStartOfDay(),
-        LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
-    visit.saveVisitToJSON();
-
-    textHealthCard.clear();
-    textName.clear();
-    textFirstSurname.clear();
-    textSecondSurname.clear();
-    maleButton.isSelected();
-    femaleButton.isSelected();
-    textDateBirth.getEditor().clear();
-    textDateVisit.getEditor().clear();
-    valueFactorySpinner();
-    setCellTable();
-    loadData(); //Llegir dades de JSON, i afegir-les a la taulas
   }
   @Override
   public void initialize(URL arg0, ResourceBundle arg1){
@@ -149,5 +151,95 @@ public class MainController implements Initializable {
     visit = new Visit();
     data = visit.loadDataFromJSON(data);
     tableVisits.setItems(data);
+  }
+
+  public boolean validationHealthCard() {
+    if(getHealthCard().equals("")){
+      AlertDialog.display("Error HeartCard", "HeartCard is required");
+      return false;
+    }
+
+    if(getHealthCard().length() != 18){
+      AlertDialog.display("Error HeartCard", "HearthCard ha de tenir exactament 18 caràcters (comptant lletres, numeros i espais) \n" +
+          "o ha de complir el format XXYY 1 234567 89 0");
+      return false;
+    }
+
+    if(!getHealthCard().matches("^\\s*[\\da-zA-Z][\\da-zA-Z\\s]*$")){
+      AlertDialog.display("Error HeartCard", "HeartCard ha de ser alfanumeric");
+      return false;
+    }
+
+    if(!getHealthCard().matches("\\w{4} \\d{1} \\d{6} \\d{2} \\d{1}")){
+      AlertDialog.display("Error HeartCard", "HearthCard ha de complir el format XXYY 1 234567 89 0.\n" +
+          "XX (les dues primeres lletres del primer cognom) i YY (les dues primeres lletres del segon cognom)");
+      return false;
+    }
+
+    String prefixHealthCard = (getHealthCard().charAt(0))+""+(getHealthCard().charAt(1))+""+(getHealthCard().charAt(2))+""+(getHealthCard().charAt(3));
+    if(!prefixHealthCard.equals(prefixHealthCard.toUpperCase())){
+      AlertDialog.display("Error HeartCard", "El prefix ha de ser en majuscules");
+      return false;
+    }
+
+    //validar cognoms (comprovar que no siguin nulls)
+    if(validateSurname()){
+      String prefixSurname = (getFirstSurname().charAt(0))+""+(getFirstSurname().charAt(1))+""+(getSecondSurname().charAt(0))+""+(getSecondSurname().charAt(1));
+
+      if(!prefixHealthCard.equals(prefixHealthCard.toUpperCase())){
+        AlertDialog.display("Error HeartCard", "El prefix HealthCard ha de ser en majuscules");
+        return false;
+      }
+
+      if(!prefixHealthCard.equals(prefixSurname.toUpperCase())){
+        AlertDialog.display("Error HeartCard", "El prefix HealthCard es diferent a les 2 lletres de cada cognom");
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public boolean validateSurname() {
+    if(getFirstSurname().equals("")){
+      AlertDialog.display("Error First Surname", "First Surnames is required");
+      return false;
+    }
+
+    if(getSecondSurname().equals("")){
+      AlertDialog.display("Error Second Surname", "Second Surnames is required");
+      return false;
+    }
+
+    if(!(getFirstSurname().length() >= 3 && getFirstSurname().length() <= 23)){
+      AlertDialog.display("Error Firt Surname", "First Surname ha de tenir mínim 3 caràcters i màxim 23 caràcters");
+      return false;
+    }
+
+    if(!(getSecondSurname().length() >= 3 && getSecondSurname().length() <= 23)){
+      AlertDialog.display("Error Second Surname", "Second Surname ha de tenir mínim 3 caràcters i màxim 23 caràcters");
+      return false;
+    }
+
+    if(!(String.valueOf(getFirstSurname().charAt(0)).toUpperCase().equals(String.valueOf(getFirstSurname().charAt(0))))){
+      AlertDialog.display("Error First Surname", "La primera lletra del First Surname ha de ser en majúscula");
+      return false;
+    }
+
+    if(!(String.valueOf(getSecondSurname().charAt(0)).toUpperCase().equals(String.valueOf(getSecondSurname().charAt(0))))){
+      AlertDialog.display("Error Second Surname", "La primera lletra del Second Surname ha de ser en majúscula");
+      return false;
+    }
+
+    if(!getFirstSurname().matches("[a-zA-Z]+")){
+      AlertDialog.display("Error First Surname", "El Firt Surname ha de ser alfabètic");
+      return false;
+    }
+
+    if(!getSecondSurname().matches("[a-zA-Z]+")){
+      AlertDialog.display("Error Second Surname", "El Second Surname ha de ser alfabètic");
+      return false;
+    }
+    return true;
   }
 }
